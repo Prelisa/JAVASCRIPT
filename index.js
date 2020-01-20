36464c5e956ffe3ea65f95efe16da6ac0598f817
Prelisa <dprelisa@gmail.com>
2020-01-20 14:13:33 +0545

    1st

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello world');
+var name;
+name= 'prelisa';
+name= 123;
+name= true;
+console.log('name is' + ' ' + name);
+
+var x= 5;
+var y= 6;
+console.log(x + y);
+
+//Operators 
+//Arithematic 
+//Relational
